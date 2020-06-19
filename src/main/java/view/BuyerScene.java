16173cb4fb199e9639c7d477bf380ecc594f8f01
ali moroukian <moroukianali@gmail.com>
2020-06-19 18:00:52 +0430

    buyer : personal info

--- src/main/java/view/BuyerScene.java
+++ src/main/java/view/BuyerScene.java
@@ -1,23 +1,36 @@
 package view;
 
+import controller.AllAccountZone;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 import static view.MainScenes.createButton;
 import static view.MainScenes.createTextField;
 import static view.MainScenes.createLabel;
 
 public class BuyerScene {
+    private static Stage openedStage;
+    private static Scene lastScene;
 
     public static Scene getBuyerScene() {
         Button personalInfo = createButton("View Personal Info", 200);
+        personalInfo.setOnMouseClicked(e -> getPersonalInfo());
+
         Button cart = createButton("View Cart", 200);
         Button orders = createButton("View Orders", 200);
-        Button balance = createButton("View Balance", 200);
         Button discountCodes = createButton("View Discount Codes", 200);
 
         Button logout = createButton("Logout", 200);
@@ -29,11 +42,94 @@ public class BuyerScene {
             CommandProcessor.getStage().setMaximized(true);
         });
 
-        VBox vBox = new VBox(25, personalInfo, cart, orders, balance, discountCodes);
+        VBox vBox = new VBox(25, personalInfo, cart, orders, discountCodes, logout, back);
         vBox.setAlignment(Pos.CENTER);
 
         Screen screen = Screen.getPrimary();
         Rectangle2D bound = screen.getBounds();
         return new Scene(vBox, bound.getWidth(), bound.getHeight());
+    }
+
+    private static void getPersonalInfo() {
+        Label firstNameLabel = createLabel("First Name : ", 150);
+        Label lastNameLabel = createLabel("Last Name : ", 150);
+        Label emailLabel = createLabel("Email : ", 150);
+        Label phoneNumberLabel = createLabel("Phone Number : ", 150);
+        Label usernameLabel = createLabel("Username : ", 150);
+        Label passwordLabel = createLabel("Password : ", 150);
+        Label walletLabel = createLabel("Balance : ", 150);
+
+        ArrayList<String> personalInfo = new ArrayList<>(AllAccountZone.getPersonalInfo());
+
+        TextField firstNameText = createTextField("First Name", 200);
+        firstNameText.setText(personalInfo.get(0));
+        firstNameText.setDisable(true);
+
+        TextField lastNameText = createTextField("Last Name", 200);
+        lastNameText.setText(personalInfo.get(1));
+        lastNameText.setDisable(true);
+
+        TextField emailText = createTextField("Email", 200);
+        emailText.setText(personalInfo.get(2));
+        emailText.setDisable(true);
+
+        TextField phoneNumberText = createTextField("Phone Number", 200);
+        phoneNumberText.setText(personalInfo.get(3));
+        phoneNumberText.setDisable(true);
+
+        TextField usernameText = createTextField("Username", 200);
+        usernameText.setText(personalInfo.get(4));
+        usernameText.setDisable(true);
+
+        TextField passwordText = createTextField("Password", 200);
+        passwordText.setText(personalInfo.get(5));
+        passwordText.setDisable(true);
+
+        TextField walletText = createTextField("Wallet", 200);
+        walletText.setText(personalInfo.get(6));
+        walletText.setDisable(true);
+
+        Button firstNameButton = createButton("Edit", 100);
+        firstNameButton.setOnMouseClicked(e -> Actions.editPersonalInfo(firstNameButton, firstNameText));
+        Button lastNameButton = createButton("Edit", 100);
+        lastNameButton.setOnMouseClicked(e -> Actions.editPersonalInfo(lastNameButton, lastNameText));
+        Button emailButton = createButton("Edit", 100);
+        emailButton.setOnMouseClicked(e -> Actions.editPersonalInfo(emailButton, emailText));
+        Button phoneNumberButton = createButton("Edit", 100);
+        phoneNumberButton.setOnMouseClicked(e -> Actions.editPersonalInfo(phoneNumberButton, phoneNumberText));
+        Button passwordButton = createButton("Edit", 100);
+        passwordButton.setOnMouseClicked(e -> Actions.editPersonalInfo(passwordButton, passwordText));
+        Button walletButton = createButton("Edit", 100);
+        walletButton.setOnMouseClicked(e -> Actions.editPersonalInfo(walletButton, walletText));
+
+        GridPane gridPane = new GridPane();
+        gridPane.addColumn(0, firstNameLabel, lastNameLabel, emailLabel, phoneNumberLabel, usernameLabel,
+                passwordLabel, walletLabel);
+        gridPane.addColumn(1, firstNameText, lastNameText, emailText, phoneNumberText, usernameText, passwordText,
+                walletText);
+        gridPane.addColumn(2, firstNameButton, lastNameButton, emailButton, phoneNumberButton);
+        gridPane.add(passwordButton, 2, 5);
+        gridPane.add(walletButton, 2, 6);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(20);
+        gridPane.setVgap(20);
+
+        ScrollPane scrollPane = new ScrollPane(gridPane);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+
+        openStage(scrollPane, "Personal Info", 600, 550);
+    }
+
+    private static Stage openStage(Parent root, String title, int width, int height) {
+        Scene scene = new Scene(root, width, height);
+        lastScene = scene;
+        Stage stage = new Stage();
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(CommandProcessor.getStage());
+        stage.show();
+        return stage;
     }
 }
